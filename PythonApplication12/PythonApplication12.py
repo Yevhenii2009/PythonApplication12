9d14f9aaa9d74cb73eeacb178e515262e581135f
B674387 <B674387@LCCB12842>
2026-06-02 11:30:40 +0100

    Add project files.

---
+++ PythonApplication12/PythonApplication12.py
@@ -0,0 +1,27 @@
+def square (x):
+    return x ** 2 
+def multiply (x):
+    return x * 2 
+def subrtact (x):
+    return x - 2 
+
+
+def combine (type):
+    if type == 'squere':
+        return  square
+    elif type == 'multiply':
+        return  multiply 
+    elif type == 'subtrct':
+        return  subrtact
+     
+        
+
+result = combine('squere')
+print(result(2))
+result = combine ('multiply') 
+print(result(2))  
+result = combine('subtrct')
+print(result(3))
+
+
+ 
